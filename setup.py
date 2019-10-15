0bad423cc85e362408bc33831d44a2d63a330ed8
Oscar Batori <oscarbatori@gmail.com>
2019-10-15 00:54:04 -0400

    peg dependency to avoid buggy mysql connector release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='doltpy',
       version='0.1',
       packages=find_packages(),
-      install_requires=['pandas>=0.25.0', 'pyarrow>=0.14.1', 'mysql-connector-python>=8.0.17', 'retry>=0.9.2'],
+      install_requires=['pandas>=0.25.0', 'pyarrow>=0.14.1', 'mysql-connector-python==8.0.17', 'retry>=0.9.2'],
       author='Liquidata',
       author_email='oscar@liquidata.co',
       description='',
