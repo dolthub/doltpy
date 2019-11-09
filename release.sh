a52cbb1aa959585cef84b6c874622073b379eca4
Oscar Batori <oscarbatori@gmail.com>
2019-11-08 19:24:11 -0800

    added release script for deplying release to PyPi

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+version=$1
+message="Are you sure you want to release doltpy version $version using commit $(git rev-parse HEAD)? [Y/y to proceed]"
+
+read -p "$message" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  printf "Building building distro dist/doltpy-$version.tar.gz\n"
+  python setup.py sdist
+
+  printf "Uploading to PyPi servers\n"
+  twine upload dist/doltpy-$version.tar.gz
+else
+  printf "Exiting, nothing to do...\n"
+fi
