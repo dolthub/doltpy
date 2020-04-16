172943e3ca1aca27ea70dc871f0735eae891eaae
Oscar Batori <oscar@liquidata.co>
2020-04-16 08:54:34 -0700

    Remove sqlalchemy dependency and update download link

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
+VERSION = '0.0.9'
+
 setup(name='doltpy',
-      version='0.0.8',
+      version=VERSION,
       packages=find_packages(),
-      install_requires=['pandas>=0.25.0', 'sqlalchemy>=1.3.8', 'mysql-connector-python==8.0.17', 'retry>=0.9.2'],
+      install_requires=['pandas>=0.25.0', 'mysql-connector-python==8.0.17', 'retry>=0.9.2'],
       author='Liquidata',
       author_email='oscar@liquidata.co',
       description='A Python package for using Dolt database via Python.',
       url='https://github.com/liquidata-inc/doltpy',
-      download_url='https://github.com/liquidata-inc/doltpy/archive/v0.0.5.tar.gz',
+      download_url='https://github.com/liquidata-inc/doltpy/archive/v{}.tar.gz'.format(VERSION),
       keywords=['Dolt', 'Liquidata', 'DoltHub', 'ETL', 'ELT'],
       project_urls={'Bug Tracker': 'https://github.com /liquidata-inc/core/issues'},
       entry_points={
