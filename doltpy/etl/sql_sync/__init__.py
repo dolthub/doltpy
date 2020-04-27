ac161d104518d7511b88f0592405024c4a8163ce
Oscar Batori <oscar@liquidata.co>
2020-04-27 12:44:51 -0700

    some code clean up

--- doltpy/etl/sql_sync/__init__.py
+++ doltpy/etl/sql_sync/__init__.py
@@ -1,2 +1,6 @@
 from .tools import TargetWriter, SourceReader, DoltSync
+from .dolt import (get_source_reader as get_dolt_source_reader,
+                   get_table_reader as get_dolt_table_reader,
+                   get_table_reader_diffs as get_dolt_table_reader_diffs)
+from .mysql import get_target_writer as get_mysql_target_writer
 
