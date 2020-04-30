afee6e9d2a1892f09c8ccd72864a830d0d8b0ddb
Oscar Batori <oscar@liquidata.co>
2020-04-30 14:14:33 -0700

    Augment doltpy.etl.sql_sync module with more functions

--- doltpy/etl/sql_sync/__init__.py
+++ doltpy/etl/sql_sync/__init__.py
@@ -1,6 +1,9 @@
 from .tools import sync_from_dolt, sync_to_dolt
 from .dolt import (get_source_reader as get_dolt_source_reader,
                    get_table_reader as get_dolt_table_reader,
-                   get_table_reader_diffs as get_dolt_table_reader_diffs)
-from .mysql import get_target_writer as get_mysql_target_writer
+                   get_table_reader_diffs as get_dolt_table_reader_diffs,
+                   get_target_writer as get_dolt_target_writer)
+from .mysql import (get_target_writer as get_mysql_target_writer,
+                    get_table_reader as get_mysql_table_reader,
+                    get_source_reader as get_mysql_source_reader)
 
