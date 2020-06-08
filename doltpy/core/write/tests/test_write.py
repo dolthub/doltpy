ebb07f9b3529f39c7cef0096871677d5671d7b61
Oscar Batori <oscar@liquidata.co>
2020-06-08 14:03:37 -0700

    Expand tests to corner cases

--- doltpy/core/write/tests/test_write.py
+++ doltpy/core/write/tests/test_write.py
@@ -2,6 +2,7 @@ from doltpy.core.write import import_dict, import_list
 from doltpy.core.read import pandas_read_sql
 import pandas as pd
 from datetime import datetime
+import pytest
 
 LIST_OF_DICTS = [
     {'name': 'Anna', 'adjective': 'tragic', 'id': 1, 'date_of_death': datetime(1877, 1, 1)},
@@ -31,3 +32,41 @@ def test_import_lists(init_empty_test_repo, run_serve_mode):
     df = pandas_read_sql(repo, 'select * from characters', repo.get_connection())
     expected = pd.DataFrame(LIST_OF_DICTS)
     assert df.equals(expected)
+
+
+LIST_OF_DICTS_WITH_NULLS = [
+    {'name': 'Roger', 'date_of_death': None},
+    {'name': 'Rafael', 'date_of_death': None},
+    {'name': 'Novak', 'date_of_death': None}
+]
+
+
+def test_import_dict_all_nulls(init_empty_test_repo, run_serve_mode):
+    repo = init_empty_test_repo
+    with pytest.raises(ValueError ):
+        import_list(repo, 'players', LIST_OF_DICTS_WITH_NULLS, ['name'], 'create')
+
+
+LIST_OF_DICTS_WITH_MISSING_KEYS = [
+    {'name': 'Roger', 'top_rank': 1},
+    {'name': 'Rafael', 'hand': 'left'},
+    {'name': 'Novak', 'top_rank': 1}
+]
+
+
+def test_import_list_missing_keys(init_empty_test_repo, run_serve_mode):
+    repo = init_empty_test_repo
+    with pytest.raises(AssertionError):
+        import_list(repo, 'players', LIST_OF_DICTS_WITH_MISSING_KEYS, ['name'], 'create')
+
+
+DICT_OF_LISTS_UNEVEN_LENGTHS = {
+    'name': ['Roger', 'Rafael', 'Novak'],
+    'rank': [1, 2]
+}
+
+
+def test_import_lists_uneven(init_empty_test_repo, run_serve_mode):
+    repo = init_empty_test_repo
+    with pytest.raises(AssertionError):
+        import_dict(repo, 'players', DICT_OF_LISTS_UNEVEN_LENGTHS, ['name'], 'create')
