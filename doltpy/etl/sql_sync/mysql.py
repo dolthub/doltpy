709154ea45a9e8ba339e1b9a695af5d0fc5e0dae
Oscar Batori <oscar@liquidata.co>
2020-08-17 19:21:47 -0700

    Support for writing array values to LONGTEXT fields in Dolt

--- doltpy/etl/sql_sync/mysql.py
+++ doltpy/etl/sql_sync/mysql.py
@@ -3,7 +3,8 @@ from sqlalchemy.dialects.mysql import insert
 from sqlalchemy import Table
 from doltpy.etl.sql_sync.db_tools import DoltAsSourceWriter, get_target_writer_helper
 from doltpy.core.system_helpers import get_logger
-from typing import List
+from typing import List, Iterable
+from datetime import datetime, date, time
 
 logger = get_logger(__name__)
 
@@ -16,7 +17,7 @@ def get_target_writer(engine: Engine, update_on_duplicate: bool = True) -> DoltA
     :param update_on_duplicate: indicates whether to update values when encountering duplicate PK, default True
     :return:
     """
-    return get_target_writer_helper(engine, upsert_helper, update_on_duplicate)
+    return get_target_writer_helper(engine, upsert_helper, update_on_duplicate, clean_types)
 
 
 def upsert_helper(table: Table, data: List[dict]):
@@ -24,4 +25,37 @@ def upsert_helper(table: Table, data: List[dict]):
     update_dict = {el.name: el for el in insert_statement.inserted if not el.primary_key}
     on_duplicate_key_statement = insert_statement.on_duplicate_key_update(update_dict)
     return on_duplicate_key_statement
+
+
+def clean_types(data: Iterable[dict]) -> List[dict]:
+    """
+    MySQL does not support native array or JSON types, additionally mysql-connector-python does not support
+    datetime.date (though that seems like a bug in the connector). This implements a very crude transformation of array
+    types and coerces datetime.date values to equivalents. This is quite an experimental feature and is currently a way
+    to transform array valued data read from Postgres to Dolt.
+    :param data:
+    :return:
+    """
+    data_copy = []
+    for row in data:
+        row_copy = {}
+        for col, val in row.items():
+            if type(val) == date:
+                row_copy[col] = datetime.combine(val, time())
+            elif type(val) == list:
+                if not val:
+                    row_copy[col] = None
+                else:
+                    row_copy[col] = ','.join(str(el) for el in val)
+            elif type(val) == dict:
+                if not val:
+                    row_copy[col] = None
+                else:
+                    row_copy[col] = str(val)
+            else:
+                row_copy[col] = val
+
+        data_copy.append(row_copy)
+
+    return data_copy
 
