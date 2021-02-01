9c138387a60915d6360502203c9112eec7e475d5
Max Hoffman <max@dolthub.com>
2021-02-01 09:53:25 -0800

    Add testing script

---
+++ scripts/run_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -xeou pipefail
+
+DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+BASE=$DIR/..
+
+if [ -x poetry ] ; then
+    echo "Install poetry"
+    echo "curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --version=1.1.0b2"
+    exit 1
+fi
+
+cd $BASE
+poetry run mypy doltpy
+poetry run black . --check --exclude tests -t py37
+poetry run pytest tests
