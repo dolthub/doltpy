cf6de5cf4f99550f50e885cf97876c9495ef583b
Max Hoffman <max@dolthub.com>
2021-02-03 19:49:28 -0800

    Add loud error if importing doltpy without dolt installed locally

---
+++ doltpy/__init__.py
@@ -0,0 +1,18 @@
+import shutil
+
+if not shutil.which("dolt"):
+    install_dolt = """`dolt` not found in `PATH`
+
+Reference: https://www.dolthub.com/docs/tutorials/installation/
+to install:
+
+> sudo bash -c 'curl -L https://github.com/dolthub/dolt/releases/latest/download/install.sh | sudo bash'
+or
+> brew install dolt
+
+and configure:
+
+> dolt config --global --add user.name <your name>
+> dolt config --global --add user.email <your email>
+"""
+    raise Exception(install_dolt)
