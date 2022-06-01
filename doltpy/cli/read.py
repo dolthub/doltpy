0043e7e1d76418ac7da39ebf372f78d81a594dd0
Maximilian Hoffman <max@dolthub.com>
2022-06-01 14:19:00 -0700

    Parquet export IR (#183)
    * Parquet export IR
    
    Exporting data through a CSV intermediary is subject to loss
    of specificity and type info. This is particularly noticable
    for read_pandas, where the resulting dataframe has every column
    of type `object` and NULLs are indistinguishable from zero values.
    
    I used a small hack to export data from Dolt into a DataFrame using parquet
    instead of CSV. This requires the pyarrow dependency.
    
    I left TODOs for improvements on the Dolt side that would make
    this code cleaner and Dolt issues for the associated features.
    
    There is one bug with NULL datetime values that I added a Dolt issue
    for.
    
    * fix fmt
    
    * revert python changes, use early pyarrow

--- doltpy/cli/read.py
+++ doltpy/cli/read.py
@@ -1,6 +1,11 @@
 import logging
+from pathlib import Path
+import random
+import string
+from tempfile import TemporaryDirectory
 from typing import List, Mapping
 import io
+
 
 from doltcli import Dolt  # type: ignore
 from doltcli.utils import (  # type: ignore
@@ -13,6 +18,12 @@ from doltcli.utils import (  # type: ignore
 )
 import pandas as pd  # type: ignore
 
+read_formats = {
+    "parquet": ".parquet",
+    "pq": ".parquet",
+    "csv": ".csv",
+}
+
 logger = logging.getLogger(__name__)
 
 
@@ -24,5 +35,27 @@ def read_pandas_sql(dolt: Dolt, sql: str) -> pd.DataFrame:
     return read_table_sql(dolt, sql, result_parser=parse_to_pandas)
 
 
-def read_pandas(dolt: Dolt, table: str, as_of: str = None) -> pd.DataFrame:
-    return read_pandas_sql(dolt, get_read_table_asof_query(table, as_of))
+def read_pandas_parquet(dolt: Dolt, table: str, asof: str = None) -> pd.DataFrame:
+    # TODO: either dolt export should support as of, or sql query should
+    #       support parquet output format
+    ab = dolt.active_branch
+    letters = string.ascii_lowercase
+    tmp_branch = "".join(random.choice(letters) for i in range(10))
+    try:
+        dolt.checkout(tmp_branch, checkout_branch=True, start_point=asof)
+        with TemporaryDirectory() as tmpdir:
+            fpath = Path(tmpdir) / "tmp.parquet"
+            dolt.table_export(table, filename=str(fpath))
+            return pd.read_parquet(fpath)
+    finally:
+        dolt.checkout(ab)
+        dolt.branch(tmp_branch, delete=True)
+
+
+def read_pandas(dolt: Dolt, table: str, as_of: str = None, fmt="csv") -> pd.DataFrame:
+    if fmt == "csv":
+        return read_pandas_sql(dolt, get_read_table_asof_query(table, as_of))
+    elif fmt == "parquet" or fmt == "pq":
+        return read_pandas_parquet(dolt, table, as_of)
+    else:
+        raise RuntimeError(f"unexpected read format: {fmt}; expected: 'parquet' or 'csv'")
