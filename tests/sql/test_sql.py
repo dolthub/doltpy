fc5e82b622ff38f817712e9c46bd2fc60c07f19f
James Cor <james@dolthub.com>
2023-01-30 13:09:31 -0800

    wrong port?

--- tests/sql/test_sql.py
+++ tests/sql/test_sql.py
@@ -100,6 +100,6 @@ def test_show_tables_engine(sql_server):
 
 def test_log_file(sql_server, tmp_path):
     log_file = tmp_path / "temp_log"
-    conf = ServerConfig(user="root", host="localhost", port="3306", log_file=log_file)
+    conf = ServerConfig(user="root", host="localhost", port="3307", log_file=log_file)
     with DoltSQLServerContext(sql_server, conf) as conn:
         assert len(log_file.open().read()) > 0
